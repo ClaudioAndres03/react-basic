1044d5804eccbc97a9c134b2b4030a92de3a7bab
Claudio Andres <ClaudioAndres03@outlook.com>
2020-11-18 14:23:17 -0400

    learning react state

--- src/App.js
+++ src/App.js
@@ -2,24 +2,54 @@ import React from 'react'
 import './App.css';
 
 
-function Helloworld(props) {
-  return (
+// function Helloworld(props) {
+//   return (
 
-    <div id="hello">
-      <h3>{props.sub}</h3>
-      {props.mytext}
-    </div>
-  );
+//     <div id="hello">
+//       <h3>{props.sub}</h3>
+//       {props.mytext}
+//     </div>
+//   );
+// }
+
+class Helloworld extends React.Component {
+
+  state = {
+    show: true
+  }
+
+  // funcion tipo flecha
+  changeState = () => {
+    this.setState({ show: !this.state.show })
+  }
+
+  render() {
+    if (this.state.show) {
+      return (
+        <div id="hello">
+          <h3>{this.props.sub}</h3>
+          {this.props.mytext}
+          <button onClick={() => this.changeState()}>Change state</button>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          Not Results
+          <button onClick={() => this.changeState()}>Change state</button>
+        </div>
+
+      )
+    }
+
+  }
 }
-
 
 function App() {
   return (
     <div className="App">
       this is my component:
-      <Helloworld mytext="Hello user 01" sub="tittle 01" />
-      <Helloworld mytext="Hello user 02" sub="tittle 02" />
-      <Helloworld mytext="Hello user 03" sub="tittle 03" />
+      <Helloworld sub="Tittle 01" mytext="User 01" />
     </div>
   );
 }
